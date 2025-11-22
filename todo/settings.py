c926fab756e06fe318d4b9d0c69c4a43b54a2164
THOR481 <shaikmeharaj481@gmail.com>
2025-11-22 19:45:31 +0530

    Initial commit

--- todo/settings.py
+++ todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'templates'
 ]
 
 MIDDLEWARE = [
