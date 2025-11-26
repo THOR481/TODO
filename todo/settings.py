20dd11e6c6bc48ec50fea5ea5fd9c7454f4d2020
THOR481 <shaikmeharaj481@gmail.com>
2025-11-26 22:55:35 +0530

    changes in css and .env

--- todo/settings.py
+++ todo/settings.py
@@ -19,7 +19,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -28,9 +27,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
-
-ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -41,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
-    'templates'
+    'app'
 ]
 
 MIDDLEWARE = [
